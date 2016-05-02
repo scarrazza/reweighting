b438cf3e775178346c9ba37523b691a6e003fb18
Stefano Carrazza <stefano.carrazza@cern.ch>
2016-05-02 19:15:03 +0200

    adding some files

---
+++ rw.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" A PDF reweighting tool """
+
+__author__ = 'Stefano Carrazza & Zahari Kassabov'
+__license__ = 'GPL'
+__version__ = '1.0.0'
+__email__ = 'stefano.carrazza@cern.ch'
+
+
+import numpy as np
+
+def main():
+    pass
+    
+def splash():
+    print("\n 8888888b.  888       888 ")
+    print(" 888   Y88b 888   o   888 ")
+    print(" 888    888 888  d8b  888 ")
+    print(" 888   d88P 888 d888b 888 ")
+    print(" 8888888P\"  888d88888b888 ")
+    print(" 888 T88b   88888P Y88888 ")
+    print(" 888  T88b  8888P   Y8888 ")
+    print(" 888   T88b 888P     Y888 ")
+    print("\n  __v%s__" % __version__)
+    print(" Authors: S.Carrazza & Z.Kassabov\n")
+
+if __name__ == "__main__":
+    splash()
+    main()
---
+++ tools.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Stefano Carrazza & Zahari Kassabov'
+__license__ = 'GPL'
+__version__ = '1.0.0'
+__email__ = 'stefano.carrazza@cern.ch'
+
+class Data():
+
+
+class Predictions():
